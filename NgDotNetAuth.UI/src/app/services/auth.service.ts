3ed9cca2d28688c7109ca0fef9af04e6f404ac3f
shalant <doug.rosenberg@gmail.com>
2022-10-17 17:48:19 -0500

    finished

---
+++ NgDotNetAuth.UI/src/app/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  public register(user: User): Observable<any> {
+    return this.http.post<any>(
+      'https://localhost:7190/api/Auth/register', 
+      user
+      );
+  }
+
+  public login(user: User): Observable<string> {
+    return this.http.post(
+      'https://localhost:7190/api/Auth/register', 
+      user, 
+      { responseType: 'text' }
+      );
+  }
+
+  public getMe(): Observable<string> {
+    return this.http.get('https://localhost:7190/api/Auth', 
+    {responseType:'text'});
+  }
+}
