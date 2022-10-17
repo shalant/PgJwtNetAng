8e7ad29c72555511df7c9aa93ca82cc0ba0f9f46
shalant <doug.rosenberg@gmail.com>
2022-10-17 15:21:25 -0500

    finished

--- JwtWebApiTutorial/JwtWebApiTutorial/User.cs
+++ JwtWebApiTutorial/JwtWebApiTutorial/User.cs
@@ -2,5 +2,9 @@
 {
     public class User
     {
+        public string UserName { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
